554aeeb6f83e06999940c597c9e17ceda2b98215
Michael Layzell <michael@thelayzells.com>
2014-09-01 14:15:48 -0400

    Meteor 0.9.0 support. Fixes #9

--- package.js
+++ package.js
@@ -1,8 +1,13 @@
 Package.describe({
-  summary: "Client-Side Device Type Detection & Template Switching with Optional Meteor-Router Support"
+  name: "mystor:device-detection",
+  summary: "Client-Side Device Type Detection & Template Switching with Optional Meteor-Router Support",
+  version: "0.2.0",
+  git: "https://github.com/mystor/meteor-device-detection.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom('METEOR@0.9.0');
+
   api.use(['meteor', 'underscore', 'deps', 'session', 'templating', 'ui'], 'client');
 
   api.add_files(['device_detection.js', 'device_helpers.js'], 'client');
